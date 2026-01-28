0a8176767c2cc396c2e8969c3cc89125e2bf7244
Jackson <dickfosjackson@gmail.com>
2026-01-28 14:16:39 +1000

    Center header nav and remove logo

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import { site } from "@/content/site";
 import { Container } from "@/components/Container";
@@ -9,18 +8,6 @@ export function SiteHeader() {
     <header className="siteHeader">
       <Container>
         <div className="siteHeaderInner">
-          <Link href="/" className="brandLogo" aria-label={site.name}>
-            <Image
-              src="/images/longlogo.png"
-              alt={site.name}
-              width={220}
-              height={44}
-              priority
-              className="brandImg"
-            />
-            <span className="srOnly">{site.name}</span>
-          </Link>
-
           <nav className="nav">
             <Link href="/#digital-products" className="navLink">
               Digital Products
