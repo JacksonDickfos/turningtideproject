cc248e9a8c350bce290465b317fcd129fc50a1a3
Jackson <dickfosjackson@gmail.com>
2026-01-28 18:55:14 +1000

    Use footerlogo copy.png in footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -11,7 +11,7 @@ export function SiteFooter() {
         <div className="siteFooterInner">
           <div className="footerLogoWrap" aria-hidden="true">
             <Image
-              src="/images/footerlogo.png"
+              src="/images/footerlogo copy.png"
               alt=""
               width={520}
               height={240}
